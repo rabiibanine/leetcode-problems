e39d103575ee20c5eafb172d5a493f5487c50905
rabiibanine <rbanine@gmail.com>
2026-09-07 21:15:08 +0100

    update solution 1 and add solution 2

---
+++ 0021-merge-two-sorted-lists/notes.md
@@ -0,0 +1,5 @@
+# Notes
+
+# Solution 1
+
+Keeps a head, a pointer, and compares both current list heads and updates the pointers accordingly.
--- 0021-merge-two-sorted-lists/solution2.py
+++ 0021-merge-two-sorted-lists/solution2.py
@@ -1,18 +1,13 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def mergeTwoLists(self, list1, list2):
-        if list1:
-            return list2
-        elif list2:
-            return list1
-
-        if list1.val <= list2.val:
-            head = list1
-            pointer = list1
-            list1 = list1.next
-        else:
-            head = list2
-            pointer = list2
-            list2 = list2.next
+        dummy = ListNode()
+        pointer = dummy
 
         while list1 and list2:
             if list1.val <= list2.val:
@@ -25,4 +20,4 @@ class Solution:
 
         pointer.next = list1 or list2
 
-        return head
+        return dummy.next
